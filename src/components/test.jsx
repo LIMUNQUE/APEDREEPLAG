49f2bf58b699a4dc436ec1dcfd5a840324c6c763
Joshua Cobos <josalcob@espol.edu.ec>
2024-02-23 10:34:09 -0500

    test

---
+++ src/components/test.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Tabs, Tab} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className="flex flex-wrap gap-4">
+      aaaaaaa
+    </div>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+            "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       height: {
@@ -12,7 +13,9 @@ module.exports = {
       },
     },
   },
+  darkMode: "class",
   plugins: [
-    require('tailwindcss-animated')
+    require('tailwindcss-animated','nextui')
   ],
+  
 };
